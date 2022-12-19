c105188ecaff01561be3cd1da372bc4fb628ef86
Jakub Zakrzewski <jz.zakrzewsky@gmail.com>
2022-12-19 23:44:43 +0100

    list wind drives;

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,8 @@
+import keyboard
+import win32api
+
+
 def await_char(char="y", msg="", func="", param=""):
-    import keyboard
     if msg == "":
         msg_out = 'Press {} to continue'.format(char.upper())
     else:
@@ -17,3 +20,9 @@ def await_char(char="y", msg="", func="", param=""):
                 else:
                     func()
             break
+
+
+def list_win_drives():
+    drives = win32api.GetLogicalDriveStrings()
+    drives = drives.split('\000')[:-1]
+    return drives
