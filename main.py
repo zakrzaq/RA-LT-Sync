a39f74434e3b993b6ee073cefaecfca6fc41fce8
Jakub Zakrzewski <jz.zakrzewsky@gmail.com>
2023-02-13 00:43:07 +0100

    main.py add scripts p.1

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from markupsafe import Markup
 
+from scripts.rtd_reports.archive_reports import archive_reports
+from scripts.rtd_reports.convert_reports import convert_reports
+from scripts.rtd_reports.filter_reports import filter_reports
+from scripts.lt_sync.split_loadfile import split_loadfile
+from scripts.lt_sync.process_ltsync import process_ltsync
+import scripts.summary as summary
+import scripts.exclusion as excl
+import scripts.extension as extn
+import scripts.utility as util
+
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
 
 
